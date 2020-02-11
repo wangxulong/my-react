068cbd1959fd176dabdb3dad0d1b88b579d137f8
wangxulong <duanxian0402@126.com>
2020-02-11 13:49:26 +0800

    init commit

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+action=$1
 
 package(){
+ echo '开始打包'
   docker run --rm -w /workspace -v `pwd`:/workspace \
        node:8.9.4-alpine \
          yarn && npm run build
+ echo '结束打包'
 }
 
 
